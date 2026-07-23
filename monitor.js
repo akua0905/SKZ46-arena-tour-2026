b1a41055f4c354f86efecbe0ae7aeb89d0459df3
akua0905 <156403452+akua0905@users.noreply.github.com>
2026-07-23 22:32:08 +0900

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -4,73 +4,86 @@ const url =
 "https://l-tike.com/concert/mevent/?mid=366800";
 
 
-https.get(
-url,
-{
-headers:{
-"User-Agent":"Mozilla/5.0"
-}
-},
-(res)=>{
+const req = https.get(
+  url,
+  {
+    headers:{
+      "User-Agent":"Mozilla/5.0",
+      "Connection":"close"
+    }
+  },
+  (res)=>{
 
-let html="";
+    console.log("Status:", res.statusCode);
 
+    let html="";
 
-res.on(
-"data",
-chunk=>{
-html+=chunk;
-}
+    res.on("data", chunk=>{
+      html += chunk;
+    });
+
+    res.on("end", ()=>{
+
+      console.log("取得完了");
+      console.log("文字数:", html.length);
+
+      const words=[
+        "千葉県",
+        "ＬａＬａ",
+        "TOKYO",
+        "arena"
+      ];
+
+      for(const word of words){
+
+        const index =
+          html.indexOf(word);
+
+        console.log(
+          word,
+          "位置:",
+          index
+        );
+
+        if(index !== -1){
+
+          console.log(
+            html.substring(
+              index-500,
+              index+1000
+            )
+          );
+
+        }
+
+      }
+
+    });
+
+  }
 );
 
 
-res.on(
-"end",
+req.setTimeout(
+30000,
 ()=>{
 
+  console.log(
+    "30秒タイムアウト"
+  );
 
-const words=[
-"千葉県",
-"ＬａＬａ",
-"TOKYO",
-"arena"
-];
-
-
-for(
-const word of words
-){
-
-let index =
-html.indexOf(word);
-
-
-console.log(
-"\n================"
-);
-
-console.log(
-word,
-"位置:",
-index
-);
-
-
-if(index !== -1){
-
-console.log(
-html.substring(
-index-1000,
-index+1500
-)
-);
-
-}
-
-}
-
+  req.destroy();
 
 });
 
+
+req.on(
+"error",
+(err)=>{
+
+  console.log(
+    "エラー:",
+    err.message
+  );
 
 });
