45fddaa3437b9ef90a22282c68f0ae10bbe0b1ac
akua0905 <156403452+akua0905@users.noreply.github.com>
2026-07-23 22:21:29 +0900

    Update monitor.js

--- monitor.js
+++ monitor.js
@@ -4,51 +4,24 @@ const url =
 "https://l-tike.com/concert/mevent/?mid=366800";
 
 
-function request(){
-
-return new Promise((resolve,reject)=>{
-
-
-const req = https.get(
-
+https.get(
 url,
-
 {
-
 headers:{
-
 "User-Agent":
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64)",
-
-"Accept":
-"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-
-"Accept-Language":
-"ja-JP,ja;q=0.9",
-
-"Connection":
-"close"
-
+"Mozilla/5.0"
 }
-
 },
-
 (res)=>{
 
 
-console.log(
-"Status:",
-res.statusCode
-);
-
-
-let data="";
+let html="";
 
 
 res.on(
 "data",
 chunk=>{
-data += chunk;
+html+=chunk;
 }
 );
 
@@ -57,130 +30,51 @@ res.on(
 "end",
 ()=>{
 
-resolve(data);
 
-}
+const words=[
+"受付中",
+"予定枚数終了",
+"受付終了",
+"発売前"
+];
 
+
+for(
+const word of words
+){
+
+
+let index =
+html.indexOf(word);
+
+
+console.log(
+"\n================"
+);
+
+console.log(
+word,
+"位置:",
+index
 );
 
 
-}
+if(index !== -1){
 
-);
-
-
-
-req.setTimeout(
-
-30000,
-
-()=>{
-
-req.destroy();
-
-reject(
-new Error(
-"timeout"
+console.log(
+html.substring(
+index-300,
+index+300
 )
 );
 
 }
 
-);
 
-
-
-req.on(
-"error",
-reject
-);
+}
 
 
 });
 
 
-}
-
-
-
-async function main(){
-
-
-for(
-let i=1;
-i<=2;
-i++
-){
-
-
-try{
-
-
-console.log(
-"試行:",
-i
-);
-
-
-const html =
-await request();
-
-
-console.log(
-"取得成功"
-);
-
-
-console.log(
-"文字数:",
-html.length
-);
-
-
-
-console.log(
-"予定枚数終了:",
-html.includes("予定枚数終了")
-);
-
-
-
-console.log(
-"受付中:",
-html.includes("受付中")
-);
-
-
-
-break;
-
-
-
-}catch(e){
-
-
-console.log(
-"失敗:",
-e.message
-);
-
-
-
-if(i===2){
-
-console.log(
-"2回失敗"
-);
-
-}
-
-}
-
-
-}
-
-
-}
-
-
-main();
+});
